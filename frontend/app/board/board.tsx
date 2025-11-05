9cf6ef3ae086105fb45309b23ae9fcf05655d668
Edward Ellis <edwardellis.egre@gmail.com>
2025-11-05 02:08:08 +0000

    Frontent displays chessboard.  Open source chess pieces obtained

---
+++ frontend/app/board/board.tsx
@@ -0,0 +1,51 @@
+export function Board() {
+    return (<svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="640"
+        height="640"
+        viewBox="0 0 80 80">
+                <rect x="0" y="0" height="80" width="80" fill="darkblue"/>
+
+                <rect x="0" y="0" height="10" width="10" fill="lightgreen" />
+                <rect x="20" y="0" height="10" width="10" fill="lightgreen" />
+                <rect x="40" y="0" height="10" width="10" fill="lightgreen" />
+                <rect x="60" y="0" height="10" width="10" fill="lightgreen" />
+
+                <rect x="10" y="10" height="10" width="10" fill="lightgreen" />
+                <rect x="30" y="10" height="10" width="10" fill="lightgreen" />
+                <rect x="50" y="10" height="10" width="10" fill="lightgreen" />
+                <rect x="70" y="10" height="10" width="10" fill="lightgreen" />
+
+                <rect x="0" y="20" height="10" width="10" fill="lightgreen" />
+                <rect x="20" y="20" height="10" width="10" fill="lightgreen" />
+                <rect x="40" y="20" height="10" width="10" fill="lightgreen" />
+                <rect x="60" y="20" height="10" width="10" fill="lightgreen" />
+
+                <rect x="10" y="30" height="10" width="10" fill="lightgreen" />
+                <rect x="30" y="30" height="10" width="10" fill="lightgreen" />
+                <rect x="50" y="30" height="10" width="10" fill="lightgreen" />
+                <rect x="70" y="30" height="10" width="10" fill="lightgreen" />
+
+                <rect x="0" y="40" height="10" width="10" fill="lightgreen" />
+                <rect x="20" y="40" height="10" width="10" fill="lightgreen" />
+                <rect x="40" y="40" height="10" width="10" fill="lightgreen" />
+                <rect x="60" y="40" height="10" width="10" fill="lightgreen" />
+
+                <rect x="10" y="50" height="10" width="10" fill="lightgreen" />
+                <rect x="30" y="50" height="10" width="10" fill="lightgreen" />
+                <rect x="50" y="50" height="10" width="10" fill="lightgreen" />
+                <rect x="70" y="50" height="10" width="10" fill="lightgreen" />
+
+                <rect x="0" y="60" height="10" width="10" fill="lightgreen" />
+                <rect x="20" y="60" height="10" width="10" fill="lightgreen" />
+                <rect x="40" y="60" height="10" width="10" fill="lightgreen" />
+                <rect x="60" y="60" height="10" width="10" fill="lightgreen" />
+
+                <rect x="10" y="70" height="10" width="10" fill="lightgreen" />
+                <rect x="30" y="70" height="10" width="10" fill="lightgreen" />
+                <rect x="50" y="70" height="10" width="10" fill="lightgreen" />
+                <rect x="70" y="70" height="10" width="10" fill="lightgreen" />
+            </svg>);
+}
+
+export default Board;
--- frontend/app/welcome/welcome.tsx
+++ frontend/app/welcome/welcome.tsx
@@ -1,5 +1,6 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import Board from "../board/board"
 
 export function Welcome() {
   return (
@@ -41,6 +42,7 @@ export function Welcome() {
             </ul>
           </nav>
         </div>
+        <Board />
       </div>
     </main>
   );
