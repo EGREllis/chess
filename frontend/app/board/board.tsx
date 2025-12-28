cb361bace97218dfb1b389ffbf1c2e8e9e42dc57
Edward Ellis <edwardellis.egre@gmail.com>
2025-12-28 13:58:17 +0000

    Render board squares as JSX SVG.

--- frontend/app/board/board.tsx
+++ frontend/app/board/board.tsx
@@ -1,50 +1,29 @@
+import React from 'react';
+
 export function Board() {
+    const lightSquares = [  {x:0,y:0},  {x:2,y:0},  {x:4,y:0},  {x:6,y:0},
+                            {x:1,y:1},  {x:3,y:1},  {x:5,y:1},  {x:7,y:1},
+                            {x:0,y:2},  {x:2,y:2},  {x:4,y:2},  {x:6,y:2},
+                            {x:1,y:3},  {x:3,y:3},  {x:5,y:3},  {x:7,y:3},
+                            {x:0,y:4},  {x:2,y:4},  {x:4,y:4},  {x:6,y:4},
+                            {x:1,y:5},  {x:3,y:5},  {x:5,y:5},  {x:7,y:5},
+                            {x:0,y:6},  {x:2,y:6},  {x:4,y:6},  {x:6,y:6},
+                            {x:1,y:7},  {x:3,y:7},  {x:5,y:7},  {x:7,y:7}
+                         ];
+    const lightSquaresSvgJsx = lightSquares.map( (point) => (
+        React.createElement('rect', {x: (point.x * 50), y: (point.y * 50), height: 50, width: 50, fill: "lightgreen"},)
+        ));
+
+    const squares = React.createElement('g',{},lightSquaresSvgJsx);
+    console.log(squares);
     return (<svg
         xmlns="http://www.w3.org/2000/svg"
         width="640"
         height="640"
-        viewBox="0 0 80 80">
-                <rect x="0" y="0" height="80" width="80" fill="darkblue"/>
+        viewBox="0 0 400 400">
+                <rect x="0" y="0" height="400" width="400" fill="darkblue"/>
 
-                <rect x="0" y="0" height="10" width="10" fill="lightgreen" />
-                <rect x="20" y="0" height="10" width="10" fill="lightgreen" />
-                <rect x="40" y="0" height="10" width="10" fill="lightgreen" />
-                <rect x="60" y="0" height="10" width="10" fill="lightgreen" />
-
-                <rect x="10" y="10" height="10" width="10" fill="lightgreen" />
-                <rect x="30" y="10" height="10" width="10" fill="lightgreen" />
-                <rect x="50" y="10" height="10" width="10" fill="lightgreen" />
-                <rect x="70" y="10" height="10" width="10" fill="lightgreen" />
-
-                <rect x="0" y="20" height="10" width="10" fill="lightgreen" />
-                <rect x="20" y="20" height="10" width="10" fill="lightgreen" />
-                <rect x="40" y="20" height="10" width="10" fill="lightgreen" />
-                <rect x="60" y="20" height="10" width="10" fill="lightgreen" />
-
-                <rect x="10" y="30" height="10" width="10" fill="lightgreen" />
-                <rect x="30" y="30" height="10" width="10" fill="lightgreen" />
-                <rect x="50" y="30" height="10" width="10" fill="lightgreen" />
-                <rect x="70" y="30" height="10" width="10" fill="lightgreen" />
-
-                <rect x="0" y="40" height="10" width="10" fill="lightgreen" />
-                <rect x="20" y="40" height="10" width="10" fill="lightgreen" />
-                <rect x="40" y="40" height="10" width="10" fill="lightgreen" />
-                <rect x="60" y="40" height="10" width="10" fill="lightgreen" />
-
-                <rect x="10" y="50" height="10" width="10" fill="lightgreen" />
-                <rect x="30" y="50" height="10" width="10" fill="lightgreen" />
-                <rect x="50" y="50" height="10" width="10" fill="lightgreen" />
-                <rect x="70" y="50" height="10" width="10" fill="lightgreen" />
-
-                <rect x="0" y="60" height="10" width="10" fill="lightgreen" />
-                <rect x="20" y="60" height="10" width="10" fill="lightgreen" />
-                <rect x="40" y="60" height="10" width="10" fill="lightgreen" />
-                <rect x="60" y="60" height="10" width="10" fill="lightgreen" />
-
-                <rect x="10" y="70" height="10" width="10" fill="lightgreen" />
-                <rect x="30" y="70" height="10" width="10" fill="lightgreen" />
-                <rect x="50" y="70" height="10" width="10" fill="lightgreen" />
-                <rect x="70" y="70" height="10" width="10" fill="lightgreen" />
+                {squares}
             </svg>);
 }
 
